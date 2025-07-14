31ce853c6775cccdb3037b30b56b0472d87c8bb1
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2025-07-14 17:19:42 +0700

    fix: reorder language entries in LIST_LANGUAGE constant

--- netflix-frontend/src/constants/listLanguage.ts
+++ netflix-frontend/src/constants/listLanguage.ts
@@ -1,10 +1,10 @@
 export const LIST_LANGUAGE = [
     {
-        name: "Bahasa Indonesia",
-        value: "id"
-    },
-    {
         name: "English",
         value: "en"
+    },
+    {
+        name: "Bahasa Indonesia",
+        value: "id"
     }
 ]
