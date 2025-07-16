ddd62b0565047b04106199e98069529f1355aecd
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2025-07-16 16:42:34 +0700

    feat: add SectionWatch component to Landing page

--- netflix-frontend/src/pages/Landing/index.tsx
+++ netflix-frontend/src/pages/Landing/index.tsx
@@ -3,12 +3,14 @@ import Layout from "../../components/layout/DefaultLayout"
 import Jumbotron from "../../components/modules/LandingPage/Jumbotron";
 import SectionEnjoy from "../../components/modules/LandingPage/SectionContent/sectionEnjoy";
 import SectionDwonload from "../../components/modules/LandingPage/SectionContent/sectionDwonload";
+import SectionWatch from "../../components/modules/LandingPage/SectionContent/sectionWatch";
 function Landing() {
   return (
     <Layout>
       <Jumbotron />
       <SectionEnjoy />
       <SectionDwonload />
+      <SectionWatch />
     </Layout>
   )
 }
